ec7fe5286b5d5d23d5434da5cfe188f990025e24
learnershigh <jingnuoy508@naver.com>
2024-01-25 21:41:53 +0900

    Fix: Windows에서 사용하는 "CreateWindow" 제거

--- engine/source/core/Window.h
+++ engine/source/core/Window.h
@@ -30,6 +30,7 @@ public:
   virtual void SetVSync(bool enabled) = 0;
   virtual bool IsVSync() const = 0;
 
+  #undef CreateWindow
   static Window* CreateWindow(const WindowProps& props = WindowProps());
 };
 } // namespace Cm
