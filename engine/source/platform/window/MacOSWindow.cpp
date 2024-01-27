76bcfca3e24327496b57d467d3914a053b4c6f26
flowjiyun <gyp2795@gmail.com>
2024-01-27 19:55:11 +0900

    feat : glfw 이벤트 콜백 셋팅

--- engine/source/platform/window/MacOSWindow.cpp
+++ engine/source/platform/window/MacOSWindow.cpp
@@ -1,6 +1,8 @@
 #include "core/Window.h"
 #include "platform/window/MacOSWindow.h"
 #include "event/ApplicationEvent.h"
+#include "event/KeyEvent.h"
+#include "event/MouseEvent.h"
 
 namespace Cm
 {
@@ -44,12 +46,74 @@ void MacOSWindow::Init( const WindowProps& props )
   glfwSetWindowUserPointer( mWindow, &mData );
   SetVSync( true );
 
+  // set Event callback
   glfwSetWindowCloseCallback( mWindow, []( GLFWwindow* window )
                               {
     WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
 
     WindowCloseEvent event;
     data.EventCallBack(event); } );
+
+  glfwSetWindowSizeCallback( mWindow, []( GLFWwindow* window, int width, int height )
+                             {
+                               WindowData& data = *(WindowData*)glfwGetWindowUserPointer( window );
+
+                               data.Width = width;
+                               data.Height = height;
+
+                               WindowResizeEvent event(width, height);
+                               data.EventCallBack( event ); } );
+
+  glfwSetKeyCallback( mWindow, []( GLFWwindow* window, int key, int scancode, int action, int mods )
+                      {
+                        WindowData& data = *(WindowData*)glfwGetWindowUserPointer( window );
+                        switch (action)
+                        {
+                          case GLFW_PRESS:
+                          {
+                            KeyPressEvent event(key, false);
+                            data.EventCallBack(event);
+                            break;
+                          }
+                          case GLFW_RELEASE:
+                          {
+                            KeyReleasEvent event(key);
+                            data.EventCallBack(event);
+                            break;
+                          }
+                          case GLFW_REPEAT:
+                          {
+                            KeyPressEvent event(key, true);
+                            data.EventCallBack(event);
+                            break;
+                          }
+                        } } );
+
+  glfwSetMouseButtonCallback( mWindow, []( GLFWwindow* window, int button, int action, int mods )
+                              {
+                                WindowData& data = *(WindowData*)glfwGetWindowUserPointer( window ); 
+                                switch (action)
+                                {
+                                  case GLFW_PRESS:
+                                  {
+                                    MouseButtonPressEvent event(button);
+                                    data.EventCallBack(event);
+                                    break;
+                                  }
+                                  case GLFW_RELEASE:
+                                  {
+                                    MouseButtonReleaseEvent event(button);
+                                    data.EventCallBack(event);
+                                    break;
+                                  }
+                                } } );
+
+  glfwSetCursorPosCallback( mWindow, []( GLFWwindow* window, double xPos, double yPos )
+                            {
+                              WindowData& data = *(WindowData*)glfwGetWindowUserPointer( window );
+
+                              MouseMoveEvent event(static_cast<float>(xPos), static_cast<float>(yPos));
+                              data.EventCallBack(event); } );
 }
 
 void MacOSWindow::Shutdown()
