afca879557a9443b92c5aab1331f413c8a04bb1b
flowjiyun <gyp2795@gmail.com>
2024-01-27 13:38:58 +0900

    fix : logger 수정 & 포멧 수정
    -fatal -> critical

--- engine/source/core/Logger.h
+++ engine/source/core/Logger.h
@@ -1,23 +1,27 @@
 #pragma once
 #ifndef CHIMERA_ENGINE_SOURCE_CORE_LOGGER_H
-#define CHIMERA_ENGINE_SOURCE_CORE_LOGGER_H
+#  define CHIMERA_ENGINE_SOURCE_CORE_LOGGER_H
 
-#include "spdlog/spdlog.h"
-#include "spdlog/fmt/ostr.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
+#  include "spdlog/spdlog.h"
+#  include "spdlog/fmt/ostr.h"
+#  include "spdlog/sinks/stdout_color_sinks.h"
 
-#include "core/Pch.h"
-#include "internal/CmUtil.h"
+#  include "core/Pch.h"
+#  include "internal/CmUtil.h"
 
-namespace Cm {
-class CHIMERA_API Logger {
+namespace Cm
+{
+class CHIMERA_API Logger
+{
 public:
   static void Init();
 
-  inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() {
+  inline static std::shared_ptr<spdlog::logger>& GetCoreLogger()
+  {
     return sCoreLogger;
   }
-  inline static std::shared_ptr<spdlog::logger>& GetClientLogger() {
+  inline static std::shared_ptr<spdlog::logger>& GetClientLogger()
+  {
     return sClientLogger;
   }
 
@@ -27,18 +31,18 @@ private:
 };
 
 // Core Logger Macro
-#define CM_CORE_TRACE(...) ::Cm::Logger::GetCoreLogger()->trace(__VA_ARGS__)
-#define CM_CORE_INFO(...) ::Cm::Logger::GetCoreLogger()->info(__VA_ARGS__)
-#define CM_CORE_WARN(...) ::Cm::Logger::GetCoreLogger()->warn(__VA_ARGS__)
-#define CM_CORE_ERROR(...) ::Cm::Logger::GetCoreLogger()->error(__VA_ARGS__)
-#define CM_CORE_FATAL(...) ::Cm::Logger::GetCoreLogger()->fatal(__VA_ARGS__)
+#  define CM_CORE_TRACE( ... ) ::Cm::Logger::GetCoreLogger()->trace( __VA_ARGS__ )
+#  define CM_CORE_INFO( ... ) ::Cm::Logger::GetCoreLogger()->info( __VA_ARGS__ )
+#  define CM_CORE_WARN( ... ) ::Cm::Logger::GetCoreLogger()->warn( __VA_ARGS__ )
+#  define CM_CORE_ERROR( ... ) ::Cm::Logger::GetCoreLogger()->error( __VA_ARGS__ )
+#  define CM_CORE_CRITICAL( ... ) ::Cm::Logger::GetCoreLogger()->critical( __VA_ARGS__ )
 
 // Client Logger Macro
-#define CM_CLIENT_TRACE(...) ::Cm::Logger::GetClientLogger()->trace(__VA_ARGS__)
-#define CM_CLIENT_INFO(...) ::Cm::Logger::GetClientLogger()->info(__VA_ARGS__)
-#define CM_CLIENT_WARN(...) ::Cm::Logger::GetClientLogger()->warn(__VA_ARGS__)
-#define CM_CLIENT_ERROR(...) ::Cm::Logger::GetClientLogger()->error(__VA_ARGS__)
-#define CM_CLIENT_FATAL(...) ::Cm::Logger::GetClientLogger()->fatal(__VA_ARGS__)
-} // namespace Cm
+#  define CM_CLIENT_TRACE( ... ) ::Cm::Logger::GetClientLogger()->trace( __VA_ARGS__ )
+#  define CM_CLIENT_INFO( ... ) ::Cm::Logger::GetClientLogger()->info( __VA_ARGS__ )
+#  define CM_CLIENT_WARN( ... ) ::Cm::Logger::GetClientLogger()->warn( __VA_ARGS__ )
+#  define CM_CLIENT_ERROR( ... ) ::Cm::Logger::GetClientLogger()->error( __VA_ARGS__ )
+#  define CM_CLIENT_CRITICAL( ... ) ::Cm::Logger::GetClientLogger()->critical( __VA_ARGS__ )
+}// namespace Cm
 
 #endif//CHIMERA_ENGINE_SOURCE_CORE_LOGGER_H
