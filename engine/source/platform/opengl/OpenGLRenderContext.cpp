62b9ed059bcc950b946425b3a3af674a122a4620
flowjiyun <gyp2795@gmail.com>
2024-02-05 17:03:21 +0900

    fix : INFO 출력

--- engine/source/platform/opengl/OpenGLRenderContext.cpp
+++ engine/source/platform/opengl/OpenGLRenderContext.cpp
@@ -12,6 +12,11 @@ void OpenGLRenderContext::Init()
   glfwMakeContextCurrent( mWindowHandle );
   int status = gladLoadGLLoader( (GLADloadproc)glfwGetProcAddress );
   CM_ASSERT_DEV( "박지윤", status, "glad init failed!" );
+
+  CM_CORE_INFO( "opengl : " );
+  CM_CORE_INFO( "  vendor : {0}", (char*)glGetString( GL_VENDOR ) );
+  CM_CORE_INFO( "  Renderer : {0}", (char*)glGetString( GL_RENDERER ) );
+  CM_CORE_INFO( "  version : {0}", (char*)glGetString( GL_VERSION ) );
 }
 
 void OpenGLRenderContext::SwapBuffer()
