fd10a68312563d3242154d8f01f21c4609e3303d
flowjiyun <gyp2795@gmail.com>
2024-03-13 13:18:00 +0900

    feat : VertexBuffer 인터페이스 생성

---
+++ engine/source/render/VertexBuffer.h
@@ -0,0 +1,21 @@
+#ifndef CHIMERA_ENGINE_SOURCE_RENDER_VERTEXBUFFER_H
+#define CHIMERA_ENGINE_SOURCE_RENDER_VERTEXBUFFER_H
+
+#include "core/Pch.h"
+#include "render/Buffer.h"
+
+namespace Cm
+{
+class CHIMERA_API VertexBuffer
+{
+public:
+  virtual ~VertexBuffer() = default;
+
+  virtual void Bind() const = 0;
+  virtual void UnBind() const = 0;
+
+  virtual void SetData( const void* data, uint32_t size ) const = 0;
+  virtual void SetLayout( const BufferLayout& layout ) = 0;
+};
+}// namespace Cm
+#endif
