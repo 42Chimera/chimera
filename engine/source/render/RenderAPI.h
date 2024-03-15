b67854b826271bded9287afc147546a763cd1fa3
flowjiyun <gyp2795@gmail.com>
2024-03-15 15:10:37 +0900

    fix : RenderAPI 메소드 추가
    - API 종류에 상관없이 요청할 수 있는 메세지 추가
    - 화면 크기 조절, 화면 색 클리어, 그리기 메세지

--- engine/source/render/RenderAPI.h
+++ engine/source/render/RenderAPI.h
@@ -1,7 +1,9 @@
 #ifndef CHIMERA_ENGINE_SOURCE_RENDERAPI_H
 #define CHIMERA_ENGINE_SOURCE_RENDERAPI_H
 
+#include <glm/glm.hpp>
 #include "core/Pch.h"
+#include "render/VertexArray.h"
 
 namespace Cm
 {
@@ -16,6 +18,13 @@ public:
 
   RenderAPI() = default;
   virtual ~RenderAPI() = default;
+
+  virtual void SetViewPort( uint32_t x, uint32_t y, uint32_t width, uint32_t height ) const = 0;
+  virtual void SetClearColor( const glm::vec4& color ) const = 0;
+  virtual void ClearColor() const = 0;
+
+  virtual void DrawElement( const std::shared_ptr<VertexArray>& vertexArray, uint32_t count ) const = 0;
+
 
   static std::unique_ptr<RenderAPI> Create();
   static API GetAPI();
