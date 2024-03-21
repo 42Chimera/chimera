0127499cc0bc2d8cf679c31e8e1ac80b88ca7093
flowjiyun <gyp2795@gmail.com>
2024-03-21 13:12:07 +0900

    fix : TODO 추가

--- engine/source/render/PerspectiveProjection.h
+++ engine/source/render/PerspectiveProjection.h
@@ -9,6 +9,8 @@ public:
   PerspectiveProjection( float fov, float aspectRatio, float near, float far );
   const glm::mat4& CalculateProjectionMaxtrix() const;
 
+
+  // TODO : viewport size 변경에 따른 aspectRatio Set 함수 필요
 private:
   float mFov;
   float mAspectRatio;
