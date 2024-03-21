318f266480b9b01bbd74c8ca436744a6ea16a465
flowjiyun <gyp2795@gmail.com>
2024-03-21 12:40:12 +0900

    feat : projection 인터페이스 구현

---
+++ engine/source/render/Projection.h
@@ -0,0 +1,15 @@
+#ifndef CHIMERA_ENGINE_SOURCE_RENDER_PROJECTION_H
+#define CHIMERA_ENGINE_SOURCE_RENDER_PROJECTION_H
+
+#include "core/Pch.h"
+#include "glm/glm.hpp"
+
+
+class CHIMERA_API Projection
+{
+public:
+  Projection() = default;
+  virtual ~Projection() = default;
+  virtual const glm::mat4& CalculateProjectionMaxtrix() const = 0;
+};
+#endif
