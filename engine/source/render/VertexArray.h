2f349367512c05884597865dcf65f43a9e6aacb3
flowjiyun <gyp2795@gmail.com>
2024-03-23 14:33:36 +0900

    fix : IndexBuffer 객체 정보 얻을 수 있도록 수정

--- engine/source/render/VertexArray.h
+++ engine/source/render/VertexArray.h
@@ -16,7 +16,7 @@ public:
 
   virtual void AddVertexBuffer( const std::shared_ptr<VertexBuffer>& vertexbuffer ) = 0;
   virtual void SetIndexBuffer( const std::shared_ptr<IndexBuffer>& indexbuffer ) = 0;
-
+  virtual const std::shared_ptr<IndexBuffer>& GetIndexBuffer() const = 0;
   static std::shared_ptr<VertexArray> Create();
 };
 }// namespace Cm
