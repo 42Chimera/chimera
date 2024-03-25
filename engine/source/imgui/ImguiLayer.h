e72be5c8d641d9642769f9001a72b973ba0f8a90
flowjiyun <gyp2795@gmail.com>
2024-03-25 17:42:26 +0900

    feat : OnUpdate 함수 추가

--- engine/source/imgui/ImguiLayer.h
+++ engine/source/imgui/ImguiLayer.h
@@ -11,6 +11,7 @@ public:
   CHIMERA_API ImguiLayer( const std::string& name );
   CHIMERA_API virtual void OnAttatch() override;
   CHIMERA_API virtual void OnDetach() override;
+  CHIMERA_API virtual void OnUpdate() override;
   CHIMERA_API virtual void OnImguiRender() override;
 
   CHIMERA_API void begin();
